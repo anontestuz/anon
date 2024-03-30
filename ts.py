9f47fe515d99211afeb1a7e3b85a9fbb4952af22
anontestuz <165475612+anontestuz@users.noreply.github.com>
2024-03-30 07:14:33 -0400

    Add files via upload

---
+++ ts.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+
+
+def kn(tanlangan_harf):
+    with open(tanlangan_harf + ".txt", 'w') as fayl:
+        fayl.write("Hacked https://t.me/Kamolov_X7VIP ")
+
+for i in range(10):
+    def tahminiy_harf_tanlash(harf_soni=1):
+        if harf_soni < 1:
+            raise ValueError("Harf soni kamida 1 bo'lishi kerak.")
+        tanlangan_harflar = ''.join(random.choices(string.ascii_lowercase, k=harf_soni)) 
+        return tanlangan_harflar
+    tanlangan_harf = tahminiy_harf_tanlash(random.randint(1,25))  
+    print("Tahminiy tanlangan harflar:", tanlangan_harf)
+    kn(tanlangan_harf)
